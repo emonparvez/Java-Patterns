041f5b474fa1333c0ad85967990f75c7442fcf6d
Emon Parvez <32875754+emonparvez@users.noreply.github.com>
2020-04-04 02:36:28 +0600

    Pyramid pattern using star is done

--- src/javapatterns/JavaPatterns.java
+++ src/javapatterns/JavaPatterns.java
@@ -15,7 +15,40 @@ public class JavaPatterns {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+
+         
+    /*      -< n-1    n   n+1 ->
+                    *
+                *          *
+              *     *      *
+        *     *     *       *    *
+    If rows is n then coloumns is 2n-1
+    
+    
+    
+    */
+
+        int n=5;
+        for(int i=1;i<=n;i++){
+            for(int j=1; j<=2*n-1;j++)
+            {
+            if(j>=n - (i-1) && j<=n + (i-1))
+            {
+                System.out.printf("*");
+            }
+            else{
+                System.out.printf(" ");
+            }
+                
+            };
+            
+                    System.out.println(" ");
+
+        };
+    }
+
+
+
     }
     
-}
+
