881cc4d0a722c96ac763bb5982604c5f699d6c82
Emon Parvez <32875754+emonparvez@users.noreply.github.com>
2020-04-05 01:27:20 +0600

    Left Triangle pattern using star is done

---
+++ src/javapatterns/LeftTriangle.java
@@ -0,0 +1,24 @@
+
+package javapatterns;
+
+
+public class LeftTriangle {
+    
+    
+    public static void main(String[] args) {
+        for(int i=1;i<=4;i++)
+        {
+            
+           for(int k=3;k>=i;k--)
+           {
+                System.out.printf(" ");
+            }
+           
+                 for(int j=1;j<=i;j++)
+                {       
+                System.out.printf("*");
+                }
+        System.out.println(" ");
+    }
+    }
+}
